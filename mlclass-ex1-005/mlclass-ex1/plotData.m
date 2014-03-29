58222afb943f16485b0a88bd65f47675edca55d7
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2014-03-29 09:02:38 +0200

    Plotting the data.

--- mlclass-ex1-005/mlclass-ex1/plotData.m
+++ mlclass-ex1-005/mlclass-ex1/plotData.m
@@ -16,10 +16,11 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-
-
-
-
+% Plot the data.
+plot(x, y, 'rx', 'MarkerSize', 10);
+% Set the y- and x- axis label.
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
 
 % ============================================================
 
