e88a582e406d8631ebc2726a2cbe6a57b57468a7
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2014-04-10 01:43:49 +0300

    Logistic Regrassion: Computing the cost function J(θ).

--- mlclass-ex2/costFunction.m
+++ mlclass-ex2/costFunction.m
@@ -20,12 +20,9 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+h = sigmoid(X * theta);
+penalty = (-y' * log(h)) - ((1-y)' * log(1 - h));
+J = 1 / m * penalty;
 
 % =============================================================
 
