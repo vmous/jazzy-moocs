8475cb6639a01654b721dc787b1a465898bfa668
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2014-04-10 23:35:18 +0300

    Logistic Regression: Implementing regularized cost function J(θ) and gradient.

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -17,10 +17,11 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+theta0 = [0; theta(2:end)];
 
-
-
-
+h = sigmoid(X * theta);
+J = (1/m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + lambda / (2*m) * theta0' * theta0;
+grad = (1/m) * (X' * (h - y) + lambda * theta0);
 
 % =============================================================
 
