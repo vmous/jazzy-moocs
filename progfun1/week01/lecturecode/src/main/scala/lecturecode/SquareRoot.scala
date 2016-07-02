82297ae291a0bbd3acb7cfa92958f3ff17a780db
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-07-02 05:00:50 +0200

    Square root code clean up using blocks and lexical scoping.

--- progfun1/week01/lecturecode/src/main/scala/lecturecode/SquareRoot.scala
+++ progfun1/week01/lecturecode/src/main/scala/lecturecode/SquareRoot.scala
@@ -13,34 +13,37 @@ object SquareRoot {
     * 
     * @return The square root of the parameter.
     */
-  def sqrt(x: Double): Double = sqrtIter(1.0, x)
+  def sqrt(x: Double): Double = {
 
-  def sqrtIter(guess: Double, x: Double): Double = {
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
-  }
+    def sqrtIter(guess: Double): Double = {
+      if (isGoodEnough(guess)) guess
+      else sqrtIter(improve(guess))
+    }
 
-  /**
-    * We need to keep the abs(guess * guess - x) proportional to x because
-    * else
-    *  - for very small numbers number whose square root we need to find might
-    *    be even smaller than the threshold value 0.001. So, relatively
-    *    speaking, that epsilon value might be huge compared to the number we
-    *    want to find.
-    *  - for very large numbers we have the opposite problem, which is that very
-    *    large floating point numbers can actually be further apart than this
-    *    epsilon value. That means in the 52-bits, the system has available for
-    *    mantissa, tt could be that the distance between one number and the next
-    *    is actually larger than 0.001. And in that case, of course the
-    *    iteration can never stop because, simply because there is no value
-    *    that's ever good enough.
-    */
-  def isGoodEnough(guess: Double, x: Double): Boolean = {
-    abs(guess * guess - x) / x < 0.001
-  }
+    /**
+      * We need to keep the abs(guess * guess - x) proportional to x because
+      * else
+      *  - for very small numbers number whose square root we need to find might
+      *    be even smaller than the threshold value 0.001. So, relatively
+      *    speaking, that epsilon value might be huge compared to the number we
+      *    want to find.
+      *  - for very large numbers we have the opposite problem, which is that very
+      *    large floating point numbers can actually be further apart than this
+      *    epsilon value. That means in the 52-bits, the system has available for
+      *    mantissa, tt could be that the distance between one number and the next
+      *    is actually larger than 0.001. And in that case, of course the
+      *    iteration can never stop because, simply because there is no value
+      *    that's ever good enough.
+      */
+    def isGoodEnough(guess: Double): Boolean = {
+      abs(guess * guess - x) / x < 0.001
+    }
 
-  def improve(guess: Double, x: Double): Double = {
-    (guess + x / guess) / 2
+    def improve(guess: Double): Double = {
+      (guess + x / guess) / 2
+    }
+
+    sqrtIter(1.0)
   }
 
 }
