858f4fba95d6be634bccfe0834c478667c81d4da
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-07-03 23:03:35 +0200

    Generalizing to both sum and product.

--- progfun1/week02/lecturecode/src/main/scala/lecturecode/SumProduct.scala
+++ progfun1/week02/lecturecode/src/main/scala/lecturecode/SumProduct.scala
@@ -2,11 +2,14 @@ package lecturecode
 
 object SumProduct {
 
-  /** Product function. */
-  def product(f: Int => Int)(a: Int, b: Int): Int = {
-    if (a > b) 1
-    else f(a) * product(f)(a + 1, b)
+  def mapReduce(f: Int => Int, combine: (Int, Int) => Int, zero: Int)(a: Int, b: Int): Int = {
+    if (a > b) zero
+    else combine(f(a), mapReduce(f, combine, zero)(a + 1, b))
   }
+
+  def sum(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x + y, 0)(a, b)
+
+  def product(f: Int => Int)(a: Int, b: Int): Int = mapReduce(f, (x, y) => x * y, 1)(a, b)
 
   /** Factorial based on product function. */
   def factorial(n: Int): Int = product(x => x)(1, n)
--- progfun1/week02/lecturecode/src/test/scala/lecturecode/SumProductSuite.scala
+++ progfun1/week02/lecturecode/src/test/scala/lecturecode/SumProductSuite.scala
@@ -10,11 +10,15 @@ class SumProductSuite extends FunSuite {
 
   import SumProduct._
 
+  test("sum of cubes of integers between 1 and 5") {
+    assert(sum(x => x * x * x)(1, 5) === 225)
+  }
+
   test("product of squares of integers between 3 and 4") {
     assert(product(x => x * x)(3, 4) === 144)
   }
 
-  test("factorial using product") {
+  test("factorial using product function") {
     assert(factorial(5) === 120)
   }
 
