8f7a238bcc9c00d7f430fb9f062c03f627fb2873
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-07-03 23:51:25 +0200

    Implementing square root function as fixed point.

--- progfun1/week02/lecturecode/src/main/scala/lecturecode/FixedPoint.scala
+++ progfun1/week02/lecturecode/src/main/scala/lecturecode/FixedPoint.scala
@@ -18,4 +18,28 @@ object FixedPoint {
     iterate(firstGuess)
   }
 
+  /* Specification of the square root function:
+   *     sqrt(x) = the number y such that y * y = x
+   * or by dividing both sides of the equation with y:
+   *     sqrt(x) = the number y such that y = x / y
+   * Concequently, sqrt(x) is a fixed point of the function (y => x / y)
+   */
+
+  /**
+    * Square root function implemented as a fixed point.
+    * 
+    * Note that the function we pass as argument to fixedPoint is
+    *     y => (y + x / y) / 2
+    * and not simply:
+    *     y => x / y
+    * If you use the later you test it with sqrt(2) you will see that is not
+    * converging as it is oscillating between values 1 and 2.
+    * To control these oscillations that we were seeing is to prevent the
+    * estimation from varying to much. One way to do this is by averaging
+    * successive values of the original sequence. So instead of going 1,2, 1,2
+    * we take the average of two successive values that would give us 1.5 and
+    * that would set us on the right path to convergence.
+    */
+  def sqrt(x: Double): Double = fixedPoint(y => (y + x / y) / 2)(1.0)
+
 }
--- progfun1/week02/lecturecode/src/test/scala/lecturecode/FixedPointSuite.scala
+++ progfun1/week02/lecturecode/src/test/scala/lecturecode/FixedPointSuite.scala
@@ -14,4 +14,12 @@ class FixedPointSuite extends FunSuite {
     assert(fixedPoint(x => 1 + x / 2)(1) === 1.999755859375)
   }
 
+  test("fixed point square root of 2") {
+    assert(sqrt(2) === 1.4142135623746899)
+  }
+
+  test("fixed point square root of 4") {
+    assert(sqrt(4) === 2.000000000000002)
+  }
+
 }
