7e7d521a220f97438a8862a1e015293f40545bd6
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-07-27 01:06:20 +0200

    Adding case classes for variables and products.
    Making show function put parentheses in the right places.

--- progfun1/week04/lecturecode/src/main/scala/lecturecode/Expr.scala
+++ progfun1/week04/lecturecode/src/main/scala/lecturecode/Expr.scala
@@ -6,17 +6,31 @@ package lecturecode
 trait Expr {
   def eval: Int = this match {
     case Number(n) => n
+    case Variable(x) => throw new Error("Cannot evaluate until \"" + x + "\" has a value") 
     case Sum(e1, e2) => e1.eval + e2.eval
+    case Prod(e1, e2) => e1.eval * e2.eval
   }
 
   def show: String = this match {
     case Number(n) => n.toString()
+    case Variable(x) => x
     case Sum(e1, e2) => e1.show + " + " + e2.show
+    case Prod(e1, e2) => (e1, e2) match {
+      case (Sum(_, _), _) => "(" + e1.show + ")" + " * " + e2.show
+      case (_, Sum(_, _)) => e1.show + " * " + "(" + e2.show + ")"
+      case (_, _) => e1.show + " * " + e2.show
+    }
   }
 }
 
 case class Number(n: Int) extends Expr {
 }
 
+case class Variable(x: String) extends Expr {
+}
+
 case class Sum(e1: Expr, e2: Expr) extends Expr {
+}
+
+case class Prod(e1: Expr, e2: Expr) extends Expr {
 }
--- progfun1/week04/lecturecode/src/test/scala/lecturecode/ExprSuit.scala
+++ progfun1/week04/lecturecode/src/test/scala/lecturecode/ExprSuit.scala
@@ -10,17 +10,50 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExprSuite extends FunSuite {
 
-  val expr1 = Sum(Number(3), Number(7))
-  val expr2 = Sum(Sum(Number(2), Number(1)), Number(7))
+  val num_expr = Number(3)
+  val var_expr = Variable("x")
+  val sum_expr1 = Sum(Number(3), Number(7))
+  val sum_expr2 = Sum(Sum(Number(2), Number(1)), Number(7))
+  val prod_expr1 = Prod(Number(2), Number(5))
+  val prod_expr2 = Prod(Prod(Number(1), Number(5)), Number(2))
+  val complex_expr1 = Prod(Sum(Number(2), Variable("x")), Number(5))
+  val complex_expr2 = Sum(Prod(Number(2), Variable("x")), Number(5))
+  val complex_expr3 = Sum(Number(2), Prod(Variable("x"), Number(5)))
+
+  test("Evaluating Number") {
+    assert(num_expr.eval === 3)
+  }
+
+  test("Evaluating Variable") {
+    intercept[Error] {
+      var_expr.eval
+    }
+  }
 
   test("Evaluating Sum") {
-    assert(expr1.eval === 10)
-    assert(expr2.eval === 10)
+    assert(sum_expr1.eval === 10)
+    assert(sum_expr2.eval === 10)
+  }
+
+  test("Evaluating Prod") {
+    assert(prod_expr1.eval === 10)
+    assert(prod_expr2.eval === 10)
   }
 
   test("Showing Sum") {
-    assert(expr1.show === "3 + 7")
-    assert(expr2.show === "2 + 1 + 7")
+    assert(sum_expr1.show === "3 + 7")
+    assert(sum_expr2.show === "2 + 1 + 7")
+  }
+
+  test("Showing Prod") {
+    assert(prod_expr1.show === "2 * 5")
+    assert(prod_expr2.show === "1 * 5 * 2")
+  }
+
+  test("Showing complex combinations of Sum and Prod") {
+    assert(complex_expr1.show === "(2 + x) * 5")
+    assert(complex_expr2.show === "2 * x + 5")
+    assert(complex_expr3.show === "2 + x * 5")
   }
 
 }
