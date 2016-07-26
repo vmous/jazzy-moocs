f0ef075d8a738a09b801c48dc04824e3af655cb0
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-07-26 23:34:32 +0200

    Adding show function to expressions.

--- progfun1/week04/lecturecode/src/main/scala/lecturecode/Expr.scala
+++ progfun1/week04/lecturecode/src/main/scala/lecturecode/Expr.scala
@@ -8,6 +8,11 @@ trait Expr {
     case Number(n) => n
     case Sum(e1, e2) => e1.eval + e2.eval
   }
+
+  def show: String = this match {
+    case Number(n) => n.toString()
+    case Sum(e1, e2) => e1.show + " + " + e2.show
+  }
 }
 
 case class Number(n: Int) extends Expr {
@@ -15,4 +20,3 @@ case class Number(n: Int) extends Expr {
 
 case class Sum(e1: Expr, e2: Expr) extends Expr {
 }
-
--- progfun1/week04/lecturecode/src/test/scala/lecturecode/ExprSuit.scala
+++ progfun1/week04/lecturecode/src/test/scala/lecturecode/ExprSuit.scala
@@ -10,9 +10,17 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ExprSuite extends FunSuite {
 
+  val expr1 = Sum(Number(3), Number(7))
+  val expr2 = Sum(Sum(Number(2), Number(1)), Number(7))
+
   test("Evaluating Sum") {
-    assert(Sum(Number(3), Number(7)).eval === 10)
-    assert(Sum(Sum(Number(2), Number(1)), Number(7)).eval === 10)
+    assert(expr1.eval === 10)
+    assert(expr2.eval === 10)
+  }
+
+  test("Showing Sum") {
+    assert(expr1.show === "3 + 7")
+    assert(expr2.show === "2 + 1 + 7")
   }
 
 }
