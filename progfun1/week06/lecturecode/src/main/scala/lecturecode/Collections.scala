7eb02b514f47f2255875cbc93a7df630d3931f06
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-08-01 00:25:48 +0300

    Implementing the scalar product of two vectors.
    Two implementation are provided. One with normal mapping of the
    resulting zipped pairs and the other using pair pattern matching.

--- progfun1/week06/lecturecode/src/main/scala/lecturecode/Collections.scala
+++ progfun1/week06/lecturecode/src/main/scala/lecturecode/Collections.scala
@@ -4,12 +4,26 @@ object Collections {
 
   /**
     * List of all combination of of numbers x and y where x is drawn from 1..M
-    * and y is drawn from 1..N
+    * and y is drawn from 1..N.
     */
   def combinations(xr: Range, yr: Range) = {
     xr flatMap (x =>
       yr map (y => (x, y))
     )
+  }
+
+  /**
+    * Scalar product of vectors.
+    */
+  def scalarProduct(xs: Vector[Double], ys: Vector[Double]): Double = {
+    (xs zip ys).map(xy => xy._1 * xy._2).sum
+  }
+
+  /**
+    * Scalar product of vectors using pair pattern matching.
+    */
+  def scalarProductPatmat(xs: Vector[Double], ys: Vector[Double]): Double = {
+    (xs zip ys).map{ case (x, y) => x * y }.sum
   }
 
 }
