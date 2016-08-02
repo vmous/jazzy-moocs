b2e099e49d65699675bc73e827eb82f54359b8a5
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-08-02 21:43:45 +0300

    Refactoring addition method for efficiency, using foldLeft.

--- progfun1/week06/lecturecode/src/main/scala/lecturecode/Polynomials.scala
+++ progfun1/week06/lecturecode/src/main/scala/lecturecode/Polynomials.scala
@@ -5,10 +5,10 @@ object Polynomials {
   class Poly(terms0: Map[Int, Double]) {
     def this(bindings: (Int, Double)*) = this(bindings.toMap)
     val terms = terms0 withDefaultValue 0.0
-    def + (other: Poly) = new Poly(terms ++ (other.terms map adjust))
-    def adjust(term: (Int, Double)): (Int, Double) = {
+    def + (other: Poly) = new Poly((other.terms foldLeft terms)(addTerm))
+    def addTerm(terms: Map[Int, Double], term: (Int, Double)): Map[Int, Double] = {
       val (exp, coeff) = term
-      exp -> (coeff + terms(exp))
+      terms + (exp -> (coeff + terms(exp)))
     }
 
     override def toString = {
