475721e72b4e6cbd6b23cab2b67debd1b56e4645
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-09-11 22:40:15 +0200

    Using "for"-expressions for generators.
    To use "for"-expressions we define map and flatMap functions for generators.

--- progfun2/week01/lecturecode/src/main/scala/lecturecode/Generators.scala
+++ progfun2/week01/lecturecode/src/main/scala/lecturecode/Generators.scala
@@ -4,20 +4,68 @@ package lecturecode
   * Generators
   */
 object Generators {
+
   trait Generator[+T] {
+    // an alias of "this"
+    self =>
+
     def generate: T
+
+    /**
+      * map definition for generators.
+      */
+    def map[S](f: T => S): Generator[S] = new Generator[S] {
+      def generate = f(self.generate) // === Generator.this.generate
+    }
+
+    /**
+      * flatMap definition for generators.
+      */
+    def flatMap[S](f: T => Generator[S]): Generator[S] = new Generator[S] {
+      def generate = f(self.generate).generate
+    }
   }
 
+  /**
+    * Generator of random integers.
+    */
   val integers = new Generator[Int] {
     val rand = new java.util.Random
     def generate = rand.nextInt()
   }
 
-  val booleans = new Generator[Boolean] {
-    def generate = integers.generate > 0
+  /**
+    * Generator of random booleans.
+    */
+  val booleans = for (i <- integers) yield i > 0
+
+  /**
+    * Generator of random pairs of objects.
+    */
+  def pairs[T, U](t: Generator[T], u: Generator[U]) = for {
+    x <- t
+    y <- u
+  } yield (x, y)
+
+  /**
+    * Identity generator.
+    */
+  def single[T](x: T): Generator[T] = new Generator[T] {
+    def generate = x
   }
 
-  val pairs = new Generator[(Int, Int)] {
-    def generate = (integers.generate, integers.generate)
+  /**
+    * Get an integer within an interval
+    */
+  def choose(lo: Int, hi: Int): Generator[Int] = {
+    for (x <- integers) yield lo + x % (hi - lo)
   }
+
+  /**
+    * Randomly choose from an arbitrarily long list of objects.
+    */
+  def oneOf[T](xs: T*): Generator[T] = {
+    for (idx <- choose(0, xs.length)) yield xs(idx)
+  }
+
 }
