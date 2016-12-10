165ad1e77d571734aa8fa0759b1aa32448e69356
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-12-10 18:13:37 +0100

    [parprog1] Proving the non-deterministic behaviour of intersection method when applied on a parallel collection.

---
+++ parprog1/week03/lecturecode/src/main/scala/lecturecode/IntersectionWrong.scala
@@ -0,0 +1,25 @@
+package lecturecode
+
+import scala.collection._
+import org.scalameter._
+
+object IntersectionWrong {
+
+  def main(args: Array[String]) {
+    def intersection(a: GenSet[Int], b: GenSet[Int]): Set[Int] = {
+      val result = mutable.Set[Int]()
+      /* The problem with this implementation is that "result", which is a
+       mutable, non-thread safe data structure. is access by different threads
+       in the assignment below. If you run this program multiple times you will
+       observe that the results returned by the parallel version of the
+       intersection bellow is non-deterministic. */
+      for (x <- a) if (b contains x) result += x
+      result
+    }
+    val seqres = intersection((0 until 1000).toSet, (0 until 1000 by 4).toSet)
+    val parres = intersection((0 until 1000).par.toSet, (0 until 1000 by 4).par.toSet)
+    log(s"Sequential result - ${seqres.size}")
+    log(s"Parallel result   - ${parres.size}")
+  }
+
+}
