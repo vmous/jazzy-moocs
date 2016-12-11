52a14960ac96a2edf8a57a1bac4d3e7c2e299459
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2016-12-11 02:07:08 +0100

    [parprog1] Solving concurrency issues of intersection method by avoiding side-effects.
    Side-effects are avoided by using the correct combinators (which in
    our case is filter).

---
+++ parprog1/week03/lecturecode/src/main/scala/lecturecode/Intersection.scala
@@ -0,0 +1,23 @@
+package lecturecode
+
+import scala.collection._
+import org.scalameter._
+
+object Intersection {
+
+  def main(args: Array[String]) {
+    def intersection(a: GenSet[Int], b: GenSet[Int]): GenSet[Int] = {
+      /* We filter the elements from a which are also contained in b.
+       Since sets in Scala also have the type A => Boolean as the "contains"
+       method (in this case here Int => Boolean), we can use the set b as an
+       argument to the filter operation. */
+      if (a.size < b.size) a.filter(b(_))
+      else b.filter(a(_))
+    }
+    val seqres = intersection((0 until 1000).toSet, (0 until 1000 by 4).toSet)
+    val parres = intersection((0 until 1000).par.toSet, (0 until 1000 by 4).par.toSet)
+    log(s"Sequential result - ${seqres.size}")
+    log(s"Parallel result   - ${parres.size}")
+  }
+
+}
